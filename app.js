e6934d5d7940b2cfd62c38db2083a609e56f6f1c
Donald Derek <donaldderek.haddad@gmail.com>
2014-10-02 13:57:02 -0400

    child

--- app.js
+++ app.js
@@ -2,62 +2,44 @@ var request = require('request');
 var parseString = require('xml2js').parseString;
 var _ = require('underscore');
 var config = require('./config');
-var serialport = require("serialport");
-var SerialPort = serialport.SerialPort;
+var exec = require('child_process').exec;
+var child;
 
-var serialPort = new SerialPort("/dev/ttyACM0", {
-    dataBits: 8,
-    parity: 'none',
-    stopBits: 1,
-    flowControl: false,
-    parser: serialport.parsers.readline("\r")
-});
+var username = config.email;
+var password = config.password;
 
-serialPort.open(function(err) {
-  if (err) console.log('failed to open: '+err);
-  else {
-    console.log('open');
-    serialPort.on('data', function(data) {
-      console.log(data);
-    });
-    serialPort.write('MIT#', function(err, results){
-        if(err) console.log(err)
-        else
-            console.log(results);
-    })
-  }
-});
-    var username = config.email;
-    var password = config.password;
+child = exec("node serial.js MIT#", function (err, stdout, stderr) {
+    console.log(stdout);
+})
 
-    function checkMail(callback){
-        request.get("https://"+username+":"+password+"@mail.google.com/gmail/feed/atom", function(error, response, body){
-            if(error)console.log(error);
-            var xml = body;
-            parseString(xml, function (err, result) {
-                if(err) console.log(err);
-                if (result !== undefined){
-                    var msgs = result.feed.entry;
-                    _.each(msgs, function(msg, i){
-                        var id = msg.id[0];
-                        if(i == 0)
-                            return callback(msg);
-                        })
-                    }
-                });
-            })
-        }
-        var msgID=0;
-        var intID=0;
-        setInterval(function () {
-            checkMail(function(data){
-                var id = data.id[0].split(":")[2];
-                msgID = id;
-                if(intID !== msgID && intID!==0 && msgID!==0){
-                    console.log("You got mail!");
-                    console.log(data);
-
+function checkMail(callback){
+    request.get("https://"+username+":"+password+"@mail.google.com/gmail/feed/atom", function(error, response, body){
+        if(error)console.log(error);
+        var xml = body;
+        parseString(xml, function (err, result) {
+            if(err) console.log(err);
+            if (result !== undefined){
+                var msgs = result.feed.entry;
+                _.each(msgs, function(msg, i){
+                    var id = msg.id[0];
+                    if(i == 0)
+                        return callback(msg);
+                    })
                 }
-            })
-            intID=msgID;
-        }, 2000);
+            });
+        })
+    }
+    var msgID=0;
+    var intID=0;
+    setInterval(function () {
+        checkMail(function(data){
+            var id = data.id[0].split(":")[2];
+            msgID = id;
+            if(intID !== msgID && intID!==0 && msgID!==0){
+                console.log("You got mail!");
+                console.log(data);
+
+            }
+        })
+        intID=msgID;
+    }, 2000);
