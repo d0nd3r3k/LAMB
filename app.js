38df48c567071dbd4549170dd1d08359d9fd4cf0
Donald Derek <donaldderek.haddad@gmail.com>
2014-10-02 11:26:52 -0400

    serial

--- app.js
+++ app.js
@@ -2,36 +2,57 @@ var request = require('request');
 var parseString = require('xml2js').parseString;
 var _ = require('underscore');
 var config = require('./config');
-var username = config.email;
-var password = config.password;
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
 
-function checkMail(callback){
-    request.get("https://"+username+":"+password+"@mail.google.com/gmail/feed/atom", function(error, response, body){
-        if(error)console.log(error);
-        var xml = body;
-        parseString(xml, function (err, result) {
-            if(err) console.log(err);
-            if (result !== undefined){
-                var msgs = result.feed.entry;
-                _.each(msgs, function(msg, i){
-                    var id = msg.id[0];
-                    if(i == 0)
-                        return callback(msg);
-                })
-            }
-        });
-    })
-}
-var msgID=0;
-var intID=0;
-setInterval(function () {
-    checkMail(function(data){
-        var id = data.id[0].split(":")[2];
-        msgID = id;
-        if(intID !== msgID && intID!==0 && msgID!==0){
-            console.log("You got mail!");
-            console.log(data);
+var serialPort = new SerialPort("/dev/ttyACM0", {
+    baudrate: 9600
+});
+serialPort.open(function (error) {
+  if ( error ) {
+    console.log('failed to open: '+error);
+  } else {
+    console.log('open');
+    serialPort.on('data', function(data) {
+      console.log('data received: ' + data);
+    });
+    serialPort.write("ls\n", function(err, results) {
+      console.log('err ' + err);
+      console.log('results ' + results);
+    });
+  }
+});
+    var username = config.email;
+    var password = config.password;
+
+    function checkMail(callback){
+        request.get("https://"+username+":"+password+"@mail.google.com/gmail/feed/atom", function(error, response, body){
+            if(error)console.log(error);
+            var xml = body;
+            parseString(xml, function (err, result) {
+                if(err) console.log(err);
+                if (result !== undefined){
+                    var msgs = result.feed.entry;
+                    _.each(msgs, function(msg, i){
+                        var id = msg.id[0];
+                        if(i == 0)
+                            return callback(msg);
+                        })
+                    }
+                });
+            })
         }
-    })
-    intID=msgID;
-}, 2000);
+        var msgID=0;
+        var intID=0;
+        setInterval(function () {
+            checkMail(function(data){
+                var id = data.id[0].split(":")[2];
+                msgID = id;
+                if(intID !== msgID && intID!==0 && msgID!==0){
+                    console.log("You got mail!");
+                    console.log(data);
+
+                }
+            })
+            intID=msgID;
+        }, 2000);
